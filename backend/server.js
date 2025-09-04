972830de3adbd6a7925113a23d75909f67ce8fca
outcastoasis <jascha.bucher55@gmail.com>
2025-09-04 22:11:34 +0200

    Test 02

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const app = express();
 // 🔐 CORS Setup
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "https://marbula-one-app.vercel.app", // z. B. https://marbula.vercel.app
+    origin: process.env.FRONTEND_URL || "https://marbula-one-app.vercel.app",
     credentials: true,
   })
 );
